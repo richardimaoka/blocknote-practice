d59ad9c13291343763f4938c4930d29724f1c799
richardimaoka <richard.s.imaoka@gmail.com>
2025-11-16 13:40:38 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,25 @@ import { useCreateBlockNote } from "@blocknote/react";
 
 export default function App() {
   // Creates a new editor instance.
-  const editor = useCreateBlockNote();
+  const editor = useCreateBlockNote({
+    initialContent: [
+      {
+        type: "paragraph",
+        content: "Welcome to this demo!",
+      },
+      {
+        type: "heading",
+        content: "This is a heading block",
+      },
+      {
+        type: "paragraph",
+        content: "This is a paragraph block",
+      },
+      {
+        type: "paragraph",
+      },
+    ],
+  });
 
   // Renders the editor instance using a React component.
   return <BlockNoteView editor={editor} />;
